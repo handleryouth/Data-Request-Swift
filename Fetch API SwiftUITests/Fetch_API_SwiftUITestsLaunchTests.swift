3733534aba1bef6789d86ff4578170e6ac8f63e0
Tony David <70279376+handleryouth@users.noreply.github.com>
2024-08-16 08:27:41 +0700

    Initial Commit

---
+++ Fetch API SwiftUITests/Fetch_API_SwiftUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Fetch_API_SwiftUITestsLaunchTests.swift
+//  Fetch API SwiftUITests
+//
+//  Created by Tony Gultom on 16/08/24.
+//
+
+import XCTest
+
+final class Fetch_API_SwiftUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
